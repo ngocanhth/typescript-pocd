43b5fa76a890ea837f35d103d4c908fcca45ecc3
ngocanh <lengocanhxd@gmail.com>
2022-08-28 22:26:59 +0700

    Update header taiwincss

--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -5,15 +5,70 @@ export interface HeaderProps {
 
 export function Header (props: HeaderProps) {
   return (
-          <header className="header">
-                <div className="container">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/styleguides">Styleguides</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/404-page-test">page not found</Link>
-                </div>
-            </header>
+          // <header className="header">
+          //       <div className="container">
+          //       <Link to="/">Home</Link>
+          //       <Link to="/login">Login</Link>
+          //       <Link to="/styleguides">Styleguides</Link>
+          //       <Link to="/register">Register</Link>
+          //       <Link to="/dashboard">Dashboard</Link>
+          //       <Link to="/404-page-test">page not found</Link>
+          //       </div>
+          //   </header>
+             <header className="header">
+             <nav className="text-white bg-black">
+                     <div className="max-w-7xl mx-auto flex justify-between items-center relative">
+                         <img width="186" src="https://aht-d1-backend.arrowhitech.net/static/img/home-new/Amtech_Charcoal_clearcut.png" alt="" />
+                         <ul className="flex font-semibold">
+                             <li className='group'>
+                                 <a href="#" className='menu-item group-hover:border-white'>Products</a>
+                                 <div className="grid grid-cols-4 w-full p-5 absolute top-full left-0 bg-black mt-14 opacity-0 invisible group-hover:opacity-100 group-hover:visible group-hover:mt-0 transition-all duration-500">
+                                     <ul className="p-2">
+                                         <li>
+                                             <a href="#" className='mega-sub-item-title'>Consumables</a>
+                                         </li>
+                                         <li>
+                                             <a href="#" className='mega-sub-item'>Equipment</a>
+                                         </li>
+                                         <li>
+                                             <a href="#" className='mega-sub-item'>On Special</a>
+                                         </li>
+                                        
+                                     </ul>
+                                 </div>
+                             </li>
+                             <li className='group relative'>
+                                 <a href="#" className='menu-item group-hover:border-white'>Products</a>
+                                 <ul className="absolute left-0 bg-black w-max mt-14 opacity-0 invisible group-hover:opacity-100 group-hover:visible group-hover:mt-0 transition-all duration-500">
+                                     <li className="sub-dropdown relative">
+                                         <a href="#" className='menu-sub-item'>Consumables</a>
+                                         <ul className="sub-dropdown-content absolute left-full top-full bg-black w-max invisible translate-all duration-500">
+                                             <li>
+                                                 <a href="#" className='menu-sub-item'>Equipment</a>
+                                             </li>
+                                             <li>
+                                                 <a href="#" className='menu-sub-item'>On Special</a>
+                                             </li>
+                                         </ul>
+                                     </li>
+                                     <li>
+                                         <a href="#" className='menu-sub-item'>Equipment</a>
+                                     </li>
+                                     <li>
+                                         <a href="#" className='menu-sub-item'>On Special</a>
+                                     </li>
+                                     
+                                 </ul>
+                             </li>
+                             <li>
+                                 <a href="#" className='menu-item'>Height Safety</a>
+                             </li>
+                             <li>
+                                 <a href="#" className='menu-item'>Physio</a>
+                             </li>
+                         </ul>
+                     </div>
+             </nav>
+        </header>
   );
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,152 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        app: "#0b1928",
+        primary: "#111f35",
+        secondary: "#20253e",
+        third: "#172533",
+        "high-light": "#ff404e",
+        "text-1": "#72859b",
+        "text-2": "#aebed3",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      scale: {
+        160: "1.6",
+        170: "1.7",
+        180: "1.8",
+        190: "1.9",
+        200: "2",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        scale_enter: "scale_enter 400ms ease-in-out forwards",
+        scale_leave: "scale_leave 300ms ease-in-out forwards",
+        skeleton: "skeleton 2s infinite",
+        "translate-y-playlist-modal-enter":
+          "translate-y-playlist-modal-enter 300ms ease-in-out forwards",
+        "translate-y-playlist-modal-leave":
+          "translate-y-playlist-modal-leave 300ms ease-in-out forwards",
+        "pulse-opacity":
+          "pulse-opacity 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        "scale-image": "scale-image 300ms ease-in-out forwards",
+      },
+      keyframes: {
+        "scale-image": {
+          "0%": {
+            opacity: "0",
+            transform: "scale(1.2)",
+            "transform-origin": "50% 50%",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1)",
+            "transform-origin": "50% 50%",
+          },
+        },
+        "pulse-opacity": {
+          "0%": {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: "0.5",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        "translate-y-playlist-modal-enter": {
+          "0%": {
+            transform: "translateX(110%) translateY(-100%) !important",
+          },
+          "100%": {
+            transform: "translateX(0%) translateY(-100%) !important",
+          },
+        },
+        "translate-y-playlist-modal-leave": {
+          "0%": {
+            transform: "translateX(0%) translateY(-100%) !important",
+          },
+          "100%": {
+            transform: "translateX(110%) translateY(-100%) !important",
+          },
+        },
+        scale_enter: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(0) rotate(0deg)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1) rotate(360deg)",
+          },
+        },
+        scale_leave: {
+          "0%": {
+            opacity: "1",
+            transform: "scale(1) rotate(360deg)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "scale(0) rotate(0deg)",
+          },
+        },
+        skeleton: {
+          "0%": {
+            opacity: 0,
+            left: "0%",
+          },
+          "50%": {
+            opacity: 1,
+            left: "100%",
+          },
+          "100%": {
+            opacity: 0,
+            left: "0%",
+          },
+        },
+      },
+    },
+    screens: {
+      xs: "375px",
+
+      sm: "480px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1440px",
+      // => @media (min-width: 1536px) { ... }
+
+      "3xl": "1900px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  variants: {
+    extend: {
+      display: ["groupd-hover"],
+      margin: ["groupd-hover, last"],
+      visibility: ["groupd-hover"],
+      borderWidth: ['last', 'hover', 'focus'],
+      opacity: ['disabled', 'group-hover', 'group-focus'],
+      backgroundColor: ['even', 'odd'],
+      pointerEvents: ['group-hover']
+    }
+  },
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwindcss-labeled-groups")(["custom", "1", "2", "3"]),
+  ],
+};
