839961e5d5b241e4f5af27abeef4f022756c684d
ngocanh <lengocanhxd@gmail.com>
2022-10-04 00:47:51 +0700

    Make ui cart button

--- src/pages/Category/components/ProductItem.tsx
+++ src/pages/Category/components/ProductItem.tsx
@@ -1,5 +1,5 @@
 import { Product } from "@/models";
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export interface IProductProps {
@@ -8,6 +8,21 @@ export interface IProductProps {
 
 export const ProductItem: React.FC<IProductProps> = ({ product }) => {
   const navigate = useNavigate();
+  const [qty, setQty] = useState(1);
+
+  const handleDecreaseQty = (qty: number) => {
+    qty -= 1;
+    setQty(qty);
+  }
+
+  const handleIncreaseQty = (qty: number) => {
+    qty += 1;
+    setQty(qty);
+  }
+
+  const handleAddToCart = () => {
+    
+  }
 
   return (
     <Link
@@ -23,6 +38,14 @@ export const ProductItem: React.FC<IProductProps> = ({ product }) => {
         />
         <div className="Product-Title">{product.name}</div>
         <div className="Product-Price">{product.price}$</div>
+        <div className="Product-Qty">
+          <span onClick={() => handleDecreaseQty} className="decrease change-qty">-</span>
+          <input type="text"/>
+          <span className="increase change-qty">+</span>
+        </div>
+        <div className="Product-Action">
+        <button onClick={handleAddToCart} className="btn-secondary add-to-cart">Add to cart</button>
+        </div>
       </div>
     </Link>
   );
